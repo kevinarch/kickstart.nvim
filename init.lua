061f0b07f8547fbecbcc4d4fbcbd44d9ed61f3ce
Kevin Arch <admin@sontieu.dev>
2025-12-01 22:19:03 +0700

    disable mini surround

--- init.lua
+++ init.lua
@@ -965,7 +965,19 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup {
+      --   mappings = {
+      --     add = 'ys', -- Add surrounding (Normal/Visual) → ysiw), ys$"
+      --     delete = 'ds', -- Delete surrounding → ds], dst, dsf
+      --     replace = 'cs', -- Change/replace surrounding → cs'", csth1<CR>
+      --     find = '', -- Not used in vim-surround style
+      --     find_left = '', -- Not used in vim-surround style
+      --     highlight = '', -- Not used in vim-surround style
+      --
+      --     suffix_last = '', -- Not needed
+      --     suffix_next = '', -- Not needed
+      --   },
+      -- }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
